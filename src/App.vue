a5e09cbd761bc4555615dbba1e373fc82f9a5f78
DenisSTK <denisdrawler@gmail.com>
2025-08-23 16:08:21 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="vue--draw__container">
+        <div class="vue--draw__canvas">
+            canvas
+        </div>
+        <div class="vue--draw__tools">
+            <button class="vue--draw__tools__item">
+                ✏️
+            </button>
+            <button class="vue--draw__tools__item">
+                🖊️
+            </button>
+            <button class="vue--draw__tools__item">
+                🖍️
+            </button>
+            <button class="vue--draw__tools__item">
+                🩹
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
