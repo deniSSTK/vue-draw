0e09657d74341427eafe384d70d2cece6e25a3ce
Maksimk9245 <40x146@gmail.com>
2025-08-23 22:48:49 +0300

    eraser work

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,10 @@
       <button class="vue--draw__tools__item" @click="selectPencil">
         ✏️
       </button>
-      <button class="vue--draw__tools__item">🖊️</button>
-      <button class="vue--draw__tools__item">🖍️</button>
-      <button class="vue--draw__tools__item" @click="clearCanvas">
-        🩹
-      </button>
+      <button class="vue--draw__tools__item" @click="">🖊️</button>
+      <button class="vue--draw__tools__item"    >🖍️</button>
+      <button class="vue--draw__tools__item" @click="selectEraser">🩹</button>
+      <button class="vue--draw__tools__item" @click="clearCanvas">X</button>
     </div>
   </div>
 </template>
@@ -26,14 +25,25 @@ import { ref, onMounted } from 'vue'
 const canvasRef = ref<HTMLCanvasElement | null>(null)
 let ctx: CanvasRenderingContext2D | null = null
 let drawing = false
-let currentTool: 'pencil' | null = null
+let currentTool: 'pencil'|'eraser' | null = null
 let lastX = 0
 let lastY = 0
-
+const selectEraser=()=>{
+  currentTool='eraser'
+  if(ctx){
+    ctx.globalCompositeOperation='destination-out'
+    ctx.lineWidth=20
+    console.log("eraser work")
+  }
+}
 const selectPencil = () => {
   currentTool = 'pencil'
+  if(ctx){
+    ctx.globalCompositeOperation='source-over'
+    ctx.strokeStyle='black'
+    ctx.lineWidth=2
+  }
 }
-
 const getMousePos = (e: MouseEvent) => {
   if (!canvasRef.value) return { x: 0, y: 0 }
   const rect = canvasRef.value.getBoundingClientRect()
@@ -42,21 +52,18 @@ const getMousePos = (e: MouseEvent) => {
     y: (e.clientY - rect.top) * (canvasRef.value.height / rect.height)
   }
 }
-
 const onPointerDown = (e: MouseEvent) => {
-  if (currentTool !== 'pencil' || !ctx) return
+  if (!ctx || !currentTool) return
   drawing = true
   const pos = getMousePos(e)
   lastX = pos.x
   lastY = pos.y
 }
-
 const onPointerUp = () => {
   drawing = false
 }
-
 const onPointerMove = (e: MouseEvent) => {
-  if (!drawing || currentTool !== 'pencil' || !ctx) return
+  if (!drawing || !ctx || !currentTool) return
   const pos = getMousePos(e)
   ctx.beginPath()
   ctx.moveTo(lastX, lastY)
@@ -65,13 +72,11 @@ const onPointerMove = (e: MouseEvent) => {
   lastX = pos.x
   lastY = pos.y
 }
-
 const clearCanvas = () => {
   if (ctx && canvasRef.value) {
     ctx.clearRect(0, 0, canvasRef.value.width, canvasRef.value.height)
   }
 }
-
 onMounted(() => {
   if (canvasRef.value) {
     canvasRef.value.width = 1920
